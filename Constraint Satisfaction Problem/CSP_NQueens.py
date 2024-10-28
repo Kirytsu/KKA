ac13874576377340d1bac28fad97261c0e6db857
Samuel Steve Mulyono <111687790+SamSteve09@users.noreply.github.com>
2024-10-28 19:44:17 +0700

    Add user input and interface for the chess board

--- Constraint Satisfaction Problem/CSP_NQueens.py
+++ Constraint Satisfaction Problem/CSP_NQueens.py
@@ -2,8 +2,8 @@ import numpy as np
 
 def is_safe(queen_positions, row, col):
     for r in range(row):
-        if (queen_positions[r] == col or 
-            queen_positions[r] - r == col - row or 
+        if (queen_positions[r] == col or
+            queen_positions[r] - r == col - row or
             queen_positions[r] + r == col + row):
             return False
     return True
@@ -27,17 +27,35 @@ def solve_n_queens(n):
 def print_solutions(solutions, shape):
     num = 1
     for solution in solutions:
-        print(f"Solution {num} {solution}:") 
-        board = np.zeros((shape, shape), dtype=int)
-        for row, col in enumerate(solution):
-            board[row, col] = 1
-        for row in board:
-            print(" ".join(map(str, row)))
-        num+=1
-        print() 
+        print(f"Solution {num} {solution}:")
+        WHITE_TILE = "██"
+        BLACK_TILE = "  "
+        QUEEN_WHITE_TILE = "█Q"
+        QUEEN_BLACK_TILE = " Q"
 
-# n x n Board
-n = 5
+        board = np.fromfunction(lambda i, j: (i + j) % 2, (shape, shape), dtype=int)
+        for row, col in enumerate(solution):
+            board[row, col] += 2
+        for row in board:
+            print(" ".join(WHITE_TILE if tile == 0 else BLACK_TILE if tile == 1 else QUEEN_WHITE_TILE
+                           if tile == 2 else QUEEN_BLACK_TILE for tile in row))
+        num+=1
+        print()
+
+def input_n(prompt="Please enter a positive number: "):
+    while True:
+        try:
+            user_input = input(prompt)
+            number = int(user_input)
+            # Check if the number is positive
+            if number > 0:
+                return number
+            else:
+                print("That's not a positive number. Try again.")
+        except ValueError:
+            print("That's not a number. Try again.")
+
+n = input_n()
 solutions = solve_n_queens(n)
 print(f"Number of solutions for {n}-Queens problem: {len(solutions)}")
 print()
