2675981dc56649ced046422c02cf9120e4704c8c
Nathanael Valen Susilo <natan.valen78@gmail.com>
2024-10-28 20:14:30 +0700

    remove space between tiles

--- Constraint Satisfaction Problem/CSP_NQueens.py
+++ Constraint Satisfaction Problem/CSP_NQueens.py
@@ -28,16 +28,16 @@ def print_solutions(solutions, shape):
     num = 1
     for solution in solutions:
         print(f"Solution {num} {solution}:")
-        WHITE_TILE = "\033[47m  \033[0m"  # White tile background
-        BLACK_TILE = "\033[40m  \033[0m"  # Black tile background
-        QUEEN_WHITE_TILE = "\033[47m\033[30m Q\033[0m"  # White background with black 'Q'
-        QUEEN_BLACK_TILE = "\033[40m\033[37m Q\033[0m"  # Black background with white 'Q'
+        WHITE_TILE = "\033[47m   \033[0m"  # White tile background
+        BLACK_TILE = "\033[40m   \033[0m"  # Black tile background
+        QUEEN_WHITE_TILE = "\033[47m\033[30m Q \033[0m"  # White background with black 'Q'
+        QUEEN_BLACK_TILE = "\033[40m\033[37m Q \033[0m"  # Black background with white 'Q'
 
         board = np.fromfunction(lambda i, j: (i + j) % 2, (shape, shape), dtype=int)
         for row, col in enumerate(solution):
             board[row, col] += 2
         for row in board:
-            print(" ".join(WHITE_TILE if tile == 0 else BLACK_TILE if tile == 1 else QUEEN_WHITE_TILE
+            print("".join(WHITE_TILE if tile == 0 else BLACK_TILE if tile == 1 else QUEEN_WHITE_TILE
                            if tile == 2 else QUEEN_BLACK_TILE for tile in row))
         num+=1
         print()
